acd39181cae4b174d98953b7251b5bcef627bf28
TK <klarrrr17@gmail.com>
2025-05-15 06:27:10 +0700

    Fix directory

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { ScheduleModule } from '@nestjs/schedule';
-import { OracleFlightModule } from './oracle-flight/oracle-flight.module';
-import { FlightInsuranceController } from './flight-insurance/flight-insurance.controller';
-import { FileUploadController } from './file-upload/file-upload.controller';
-// import { OracleController } from './oracle/oracle.controller';
-import { FlightInsuranceService } from './flight-insurance/flight-insurance.service';
-import { SupabaseService } from './file-upload/supabase.service';
-// import { OracleService } from './oracle/oracle.service';
-import { PolicyCleanupService } from './flight-insurance/policy-cleanup.service';
+import { OracleFlightModule } from './module/oracle-flight/oracle-flight.module';
+import { FlightInsuranceController } from './module/flight-insurance/flight-insurance.controller';
+import { FileUploadController } from './module/file-upload/file-upload.controller';
+import { OracleController } from './module/oracle/oracle.controller';
+import { FlightInsuranceService } from './module/flight-insurance/flight-insurance.service';
+import { SupabaseService } from './module/file-upload/supabase.service';
+import { OracleService } from './module/oracle/oracle.service';
+import { PolicyCleanupService } from './module/flight-insurance/policy-cleanup.service';
 import { PriceModule } from './module/price/price.module';
 import { OracleModule } from './module/oracle/oracle.module';
 import { WalletModule } from './module/wallet/wallet.module';
@@ -29,12 +29,12 @@ import { AuthModule } from './module/auth/auth.module';
   controllers: [
     FlightInsuranceController,
     FileUploadController,
-    // OracleController,
+    OracleController,
   ],
   providers: [
     FlightInsuranceService,
     SupabaseService,
-    // OracleService,
+    OracleService,
     PolicyCleanupService,
     PriceModule,
     OracleModule,
